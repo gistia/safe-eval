055fd524f0916edd819db90e8fa50f56aee02d4a
Felipe Coury <felipe.coury@gmail.com>
2017-08-31 20:11:53 -0300

    fix: uses ES5

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-module.exports = (expr, context={}) => {
+module.exports = function(expr, context) {
   if (!expr) { return; }
   var key = 'GISTIA_SAFE_EVAL_' + Math.floor(Math.random() * 1000000);
+  context = context || {};
   context[key] = undefined;
   with(context) {
-    eval(`${key} = ${expr.trim()};`);
+    var expr = key + ' = ' + expr.trim() + ';';
+    eval(expr);
   }
   return context[key];
 };
